cc6edc026d33e003bd5e15afb33a345b956a76b5
Phongsakorn Wisetthon <boy1556@hotmail.com>
2020-07-04 14:01:10 +0700

    wow

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 375,
+    height: 700,
+    resizable: false,
     webPreferences: {
       nodeIntegration: true,
       webviewTag: true,
